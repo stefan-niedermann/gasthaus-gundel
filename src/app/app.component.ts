d9db94fa0378b8b3cdc33b235d54459ce422c90e
Stefan Niedermann <info@niedermann.it>
2017-08-15 19:16:11 +0200

    Theme and MDI support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, HostListener, AfterViewInit } from '@angular/core';
+import { MdSidenav } from "@angular/material";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements AfterViewInit {
+  @ViewChild(MdSidenav) sidenav: MdSidenav;
+
+  ngAfterViewInit(): void {
+    this.onResize({ target: window });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    if (event.target.screen.width > 768) {
+      this.sidenav.mode = 'side';
+      this.sidenav.open();
+    } else {
+      this.sidenav.mode = 'over';
+      this.sidenav.close();
+    }
+  }
+
+  closeSidenavOnNavigate(): void {
+    if (window.screen.width <= 768) {
+      this.sidenav.close();
+    }
+  }
 }

7b2e1ebedc368fb39f0c49e7a0b0596e31f924c5
Stefan Niedermann <info@niedermann.it>
2017-09-15 15:24:56 +0200

    Added images for speisekarte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,52 +11,54 @@ import { SpeisekarteComponent } from './speisekarte/speisekarte.component';
 import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { OeffnungszeitenPipe } from './oeffnungszeiten/oeffnungszeiten.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Route[] = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'oeffnungszeiten',
-    component: OeffnungszeitenComponent
-  },
-  {
-    path: 'anfahrt',
-    component: AnfahrtComponent
-  },
-  {
-    path: 'speisekarte',
-    component: SpeisekarteComponent
-  },
-  {
-    path: '**',
-    redirectTo: '/'
-  }
+	{
+		path: '',
+		component: HomeComponent
+	},
+	{
+		path: 'oeffnungszeiten',
+		component: OeffnungszeitenComponent
+	},
+	{
+		path: 'anfahrt',
+		component: AnfahrtComponent
+	},
+	{
+		path: 'speisekarte',
+		component: SpeisekarteComponent
+	},
+	{
+		path: '**',
+		redirectTo: '/'
+	}
 ]
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    OeffnungszeitenComponent,
-    AnfahrtComponent,
-    SpeisekarteComponent,
-    OeffnungszeitenPipe
-  ],
-  imports: [
-    BrowserModule,
-    MaterialModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forRoot(routes)
-  ],
-  providers: [
-    {
-      provide: LOCALE_ID,
-      useValue: 'de-DE'
-    }
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		HomeComponent,
+		OeffnungszeitenComponent,
+		AnfahrtComponent,
+		SpeisekarteComponent,
+		OeffnungszeitenPipe
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		MaterialModule,
+		CommonModule,
+		FormsModule,
+		RouterModule.forRoot(routes)
+	],
+	providers: [
+		{
+			provide: LOCALE_ID,
+			useValue: 'de-DE'
+		}
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }

a3d30dc39ca85d9c70e7859094450bb75a8a43b5
Stefan Niedermann <info@niedermann.it>
2017-09-16 20:36:50 +0200

    Added some links at home component and fix speisekarte graphik

--- src/app/speisekarte/speisekarte.component.ts
+++ src/app/speisekarte/speisekarte.component.ts
@@ -2,30 +2,31 @@ import { Component } from '@angular/core';
 import { Kategorie, kategorien, Gericht } from "app/speisekarte/speisekarte";
 
 @Component({
-  selector: 'app-speisekarte',
-  templateUrl: './speisekarte.component.html',
-  styleUrls: ['./speisekarte.component.less']
+	selector: 'app-speisekarte',
+	templateUrl: './speisekarte.component.html',
+	styleUrls: ['./speisekarte.component.less']
 })
 export class SpeisekarteComponent {
 
-  filteredKategorien: Kategorie[] = Array.apply([], kategorien);
+	filteredKategorien: Kategorie[] = Array.apply([], kategorien);
 
-  constructor() { }
+	constructor() { }
 
-  filter(event): void {
-    const term: string = event.target.value.trim().toLowerCase();
-    this.filteredKategorien = new Array<Kategorie>();
-    kategorien.map((kategorie: Kategorie) => {
-      const gerichteMatches: Gericht[] = kategorie.gerichte.filter((gericht: Gericht) => {
-        return gericht.titel.trim().toLowerCase().indexOf(term) >= 0
-          || gericht.beschreibung.trim().toLowerCase().indexOf(term) >= 0;
-      });
-      if (gerichteMatches.length > 0) {
-        this.filteredKategorien.push({
-          titel: kategorie.titel,
-          gerichte: gerichteMatches
-        });
-      }
-    });
-  }
+	filter(event): void {
+		const term: string = event.target.value.trim().toLowerCase();
+		this.filteredKategorien = new Array<Kategorie>();
+		kategorien.map((kategorie: Kategorie) => {
+			const gerichteMatches: Gericht[] = kategorie.gerichte.filter((gericht: Gericht) => {
+				return gericht.titel.trim().toLowerCase().indexOf(term) >= 0
+					|| gericht.beschreibung.trim().toLowerCase().indexOf(term) >= 0;
+			});
+			if (gerichteMatches.length > 0) {
+				this.filteredKategorien.push({
+					titel: kategorie.titel,
+					graphik: kategorie.graphik,
+					gerichte: gerichteMatches
+				});
+			}
+		});
+	}
 }

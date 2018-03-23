2cbfc9fd6a1b8eab0a1a2a63ac0efa4632c1accf
Stefan Niedermann <info@niedermann.it>
2018-03-23 13:50:30 +0100

    Fix tests

--- src/app/oeffnungszeiten/oeffnungszeiten.component.spec.ts
+++ src/app/oeffnungszeiten/oeffnungszeiten.component.spec.ts
@@ -1,11 +1,17 @@
+import { registerLocaleData } from '@angular/common';
 import { OeffnungszeitenPipe } from './oeffnungszeiten.pipe';
 import { MaterialModule } from 'app/material/material.module';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import localeDe from '@angular/common/locales/de';
+import localeDeExtra from '@angular/common/locales/extra/de';
 
 import { OeffnungszeitenComponent } from './oeffnungszeiten.component';
 import { AppService } from 'app/app.service';
 
 describe('OeffnungszeitenComponent', () => {
+
+	registerLocaleData(localeDe, localeDeExtra);
+
 	let component: OeffnungszeitenComponent;
 	let fixture: ComponentFixture<OeffnungszeitenComponent>;
 

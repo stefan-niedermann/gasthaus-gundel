714f55b61ee3dc864700db1ce2eabf451f847361
Niedermann IT-Dienstleistungen <info@niedermann.it>
2018-12-12 23:08:24 +0100

    Fix unit test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,12 +2,12 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { AppService } from './app.service';
 import { Angulartics2Module } from 'angulartics2';
 import { Angulartics2Piwik } from 'angulartics2/piwik';
-import { RouterModule } from '@angular/router';
 import { MaterialModule } from 'app/material/material.module';
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
       ],
       imports: [
         MaterialModule,
-        RouterModule.forRoot([]),
+        RouterTestingModule,
         Angulartics2Module,
         NoopAnimationsModule
       ],
       providers: [
         { provide: APP_BASE_HREF, useValue: '/' },
-        { provide: Angulartics2Piwik, useValue: {} },
+        { provide: Angulartics2Piwik, useValue: { startTracking: () => {}} },
         AppService
       ]
     }).compileComponents();

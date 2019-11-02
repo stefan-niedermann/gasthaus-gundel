15388a8389e456468f115633ee51770e598da514
stefan-niedermann <info@niedermann.it>
2019-11-02 23:40:01 +0100

    Initial materialize css commit

--- src/app/oeffnungszeiten/oeffnungszeiten.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OeffnungszeitenPipe } from './oeffnungszeiten.pipe';
-
-describe('OeffnungszeitenPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OeffnungszeitenPipe();
-    expect(pipe).toBeTruthy();
-  });
-});

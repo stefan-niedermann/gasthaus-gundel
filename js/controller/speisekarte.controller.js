609e76def89b88d0d45b6cb62534326673f49e5e
Stefan Niedermann <info@niedermann.it>
2024-07-04 16:13:16 +0200

    feat: Optimize deko graphics

--- js/controller/speisekarte.controller.js
+++ js/controller/speisekarte.controller.js
@@ -11,71 +11,19 @@ export class SpeisekarteController {
         const categoryForm = document.createElement('form');
         const categoryList = document.createElement('ul');
         categoryList.tabIndex = -1;
-
-        const applyFilter = value => {
-            if (value === '') {
-                Array.from(scope.querySelectorAll(`section`))
-                    .filter(section => section.hasAttribute('hidden'))
-                    .forEach(section => section.removeAttribute('hidden'));
-
-                categoryList.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
-
-            } else {
-                scope
-                    .querySelector(`section[data-title="${value}"]`)
-                    .removeAttribute('hidden');
-
-                Array.from(scope.querySelectorAll(`section:not([data-title="${value}"])`))
-                    .forEach(section => section.setAttribute('hidden', 'until-found'));
-
-                const targetLabel = scope.querySelector(`form label:has(input[value="${value}"])`);
-                const viewportWidth = parseFloat(getComputedStyle(categoryList).width);
-                const labelWidth = targetLabel.clientWidth;
-                const labelOffset = targetLabel.offsetLeft;
-                const offsetLeft = labelOffset + (labelWidth / 2) - (viewportWidth / 2);
-
-                categoryList.scrollTo({ top: 0, left: offsetLeft, behavior: 'smooth' });
-                window.scrollTo({top: scope.offsetTop});
-            }
-        };
-
         categoryForm.appendChild(categoryList);
-        categoryForm.addEventListener('change', e => applyFilter(e.target.value));
+        categoryForm.addEventListener('change', e => this.#applyFilter(scope, categoryList, e.target.value));
 
-        const createFilter = (title, value) => {
-            const categoryItem = document.createElement('li');
-            const categoryInput = document.createElement('input');
-            const categoryLabel = document.createElement('label');
-
-            categoryInput.type = 'radio';
-            categoryInput.name = 'speisekarte';
-            categoryInput.value = value;
-            categoryLabel.tabIndex = 0;
-            categoryLabel.textContent = title;
-            categoryLabel.addEventListener('keydown', event => {
-                switch (event.key) {
-                    case ' ':
-                    case 'Enter': {
-                        categoryInput.click();
-                        event.preventDefault();
-                    }
-                }
-            })
-            categoryLabel.appendChild(categoryInput);
-            categoryItem.appendChild(categoryLabel);
-
-            return categoryItem;
-        }
-
-        const filterAll = createFilter('Alle', '');
+        const filterAll = this.#createFilter('Alle', '');
         categoryList.appendChild(filterAll);
+
         Array.from(scope.querySelectorAll('section'))
             .toSorted((a, b) => b.dataset.weight || 0 - a.dataset.weight || 0)
             .forEach(section => {
                 const categoryTitle = section.querySelector('h2').textContent.trim();
                 section.dataset.title = categoryTitle;
 
-                const filter = createFilter(categoryTitle, categoryTitle);
+                const filter = this.#createFilter(categoryTitle, categoryTitle);
                 categoryList.appendChild(filter);
             });
 
@@ -91,6 +39,59 @@ export class SpeisekarteController {
             .splice(0, this.#observers.length)
             .forEach(observer => observer.disconnect());
     }
+
+    #createFilter = (title, value) => {
+        const categoryItem = document.createElement('li');
+        const categoryInput = document.createElement('input');
+        const categoryLabel = document.createElement('label');
+
+        categoryInput.type = 'radio';
+        categoryInput.name = 'speisekarte';
+        categoryInput.value = value;
+        categoryLabel.tabIndex = 0;
+        categoryLabel.textContent = title;
+        categoryLabel.addEventListener('keydown', event => {
+            switch (event.key) {
+                case ' ':
+                case 'Enter': {
+                    categoryInput.click();
+                    event.preventDefault();
+                }
+            }
+        })
+        categoryLabel.appendChild(categoryInput);
+        categoryItem.appendChild(categoryLabel);
+
+        return categoryItem;
+    }
+
+    #applyFilter(scope, categoryList, value) {
+        if (value === '') {
+            Array.from(scope.querySelectorAll(`section`))
+                .filter(section => section.hasAttribute('hidden'))
+                .forEach(section => section.removeAttribute('hidden'));
+
+            categoryList.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+
+        } else {
+            scope
+                .querySelector(`section[data-title="${value}"]`)
+                .removeAttribute('hidden');
+
+            Array.from(scope.querySelectorAll(`section:not([data-title="${value}"])`))
+                .forEach(section => section.setAttribute('hidden', 'until-found'));
+
+            const targetLabel = scope.querySelector(`form label:has(input[value="${value}"])`);
+            const viewportWidth = parseFloat(getComputedStyle(categoryList).width);
+            const labelWidth = targetLabel.clientWidth;
+            const labelOffset = targetLabel.offsetLeft;
+            const offsetLeft = labelOffset + (labelWidth / 2) - (viewportWidth / 2);
+
+            categoryList.scrollTo({ top: 0, left: offsetLeft, behavior: 'smooth' });
+        }
+        
+        window.scrollTo({ top: scope.offsetTop, behavior: 'smooth' });
+    };
 
     #initHorizontalFilterObserver(element) {
         const CLASS_ENTERED_VIEWPORT = 'entered-viewport';

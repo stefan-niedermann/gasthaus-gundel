74f0b12514d9bf7bf230126ecf9384fdb60459fc
Stefan Niedermann <info@niedermann.it>
2026-01-10 22:44:08 +0100

    debug(ci)

--- .github/scripts/get-info-box.js
+++ .github/scripts/get-info-box.js
@@ -1,9 +1,17 @@
+const announcment = process.env.ANNOUNCMENT;
+const vacationFrom = process.env.VACATION_FROM;
+const vacationTo = process.env.VACATION_TO;
 
+if (!announcment && !vacationFrom && !vacationTo) {
+    return;
+}
+
+return;
 
 
 console.log('Real undefined:', process.env.ANNOUNCMENT === undefined, 'ANNOUNCMENT:', process.env.ANNOUNCMENT, 'VACATION_FROM:', process.env.VACATION_FROM, 'VACATION_TO:', process.env.VACATION_TO);
-            // # <div class="info-box">
-            // #     <h2>Betriebsurlaub</h2>
-            // #     <p><strong>Vom 12. Januar bis zum 22. Januar sind wir im Urlaub!</strong><br>Wir freuen uns darauf, Sie ab dem 23. Januar
-            // #         wieder mit unseren griechischen Spezialitäten verwöhnen zu dürfen.</p>
-            // # </div>
+// # <div class="info-box">
+// #     <h2>Betriebsurlaub</h2>
+// #     <p><strong>Vom 12. Januar bis zum 22. Januar sind wir im Urlaub!</strong><br>Wir freuen uns darauf, Sie ab dem 23. Januar
+// #         wieder mit unseren griechischen Spezialitäten verwöhnen zu dürfen.</p>
+// # </div>

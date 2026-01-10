cef9229986b8a21b7a7ea2c8174b8980ccec4ed3
Stefan Niedermann <info@niedermann.it>
2026-01-10 21:57:45 +0100

    fix(ci)

--- .github/actions/get-info-box/index.js
+++ .github/actions/get-info-box/index.js
@@ -1,4 +1,6 @@
-console.log(process.env);
+console.log('ANNANOUCMENT:', process.env.ANNANOUCMENT);
+console.log('VACATION_FROM:', process.env.VACATION_FROM);
+console.log('VACATION_TO:', process.env.VACATION_TO);
             // # <div class="info-box">
             // #     <h2>Betriebsurlaub</h2>
             // #     <p><strong>Vom 12. Januar bis zum 22. Januar sind wir im Urlaub!</strong><br>Wir freuen uns darauf, Sie ab dem 23. Januar

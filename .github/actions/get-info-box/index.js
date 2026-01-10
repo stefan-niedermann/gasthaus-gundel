2cfef61de02f7ae352178917e44a757c2224a767
Stefan Niedermann <info@niedermann.it>
2026-01-10 21:00:43 +0100

    feat(ci): Test index.js for info box generation

---
+++ .github/actions/get-info-box/index.js
@@ -0,0 +1,7 @@
+console.log('Halloooooo-----------------');
+console.log(process.args);
+            // # <div class="info-box">
+            // #     <h2>Betriebsurlaub</h2>
+            // #     <p><strong>Vom 12. Januar bis zum 22. Januar sind wir im Urlaub!</strong><br>Wir freuen uns darauf, Sie ab dem 23. Januar
+            // #         wieder mit unseren griechischen Spezialitäten verwöhnen zu dürfen.</p>
+            // # </div>

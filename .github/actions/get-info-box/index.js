280700be3143f76acb55dda50d2060428d00fc32
Stefan Niedermann <info@niedermann.it>
2026-01-10 21:48:21 +0100

    chore(ci): Implement composite action in to avoid setting up a package.json

--- .github/actions/get-info-box/index.js
+++ .github/actions/get-info-box/index.js
@@ -1,4 +1,4 @@
-console.log(process.argv);
+console.log(process.env);
             // # <div class="info-box">
             // #     <h2>Betriebsurlaub</h2>
             // #     <p><strong>Vom 12. Januar bis zum 22. Januar sind wir im Urlaub!</strong><br>Wir freuen uns darauf, Sie ab dem 23. Januar
